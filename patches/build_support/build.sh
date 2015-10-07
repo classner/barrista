260a8b2578968d4fc9dbe230876921d491a7c137
Christoph Lassner <mail@christophlassner.de>
2015-10-07 03:18:26 +0200

    Updated travis build.

---
+++ patches/build_support/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Script called by Travis to build and test Caffe.
+# Travis CI tests are CPU-only for lack of compatible hardware.
+
+set -e
+MAKE="make --jobs=$NUM_THREADS --keep-going"
+
+mkdir build
+cd build
+CPU_ONLY=" -DCPU_ONLY=ON"
+if ! $WITH_CUDA; then
+  CPU_ONLY=" -DCPU_ONLY=OFF"
+fi
+PYTHON_ARGS=""
+if [ "$PYTHON_VERSION" = "3" ]; then
+  PYTHON_ARGS="$PYTHON_ARGS -Dpython_version=3 -DBOOST_LIBRARYDIR=$CONDA_DIR/lib/"
+fi
+if $WITH_IO; then
+  IO_ARGS="-DUSE_OPENCV=ON -DUSE_LMDB=ON -DUSE_LEVELDB=ON"
+else
+  IO_ARGS="-DUSE_OPENCV=OFF -DUSE_LMDB=OFF -DUSE_LEVELDB=OFF"
+fi
+cmake -DBUILD_python=ON -DCMAKE_BUILD_TYPE=Release $CPU_ONLY $PYTHON_ARGS -DCMAKE_INCLUDE_PATH="$CONDA_DIR/include/" -DCMAKE_LIBRARY_PATH="$CONDA_DIR/lib/" $IO_ARGS ..
+$MAKE
+$MAKE install
+cd -
