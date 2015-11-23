6f19c2477d9f3ca6e5e15091672c01b44e47bd90
Christoph Lassner <mail@christophlassner.de>
2015-11-23 15:50:38 +0100

    Fix python 2 build issue.

--- patches/build_support/travis_setup.sh
+++ patches/build_support/travis_setup.sh
@@ -52,8 +52,10 @@ if [ ! -d $CONDA_DIR ]; then
 	./miniconda.sh -b -p $CONDA_DIR
 
 	conda update --yes conda
-  # The version of boost we're using for Python 3 depends on 3.4 for now.
-  conda install --yes python=3.4
+  if [ "$PYTHON_VERSION" -eq "3" ]; then
+    # The version of boost we're using for Python 3 depends on 3.4 for now.
+    conda install --yes python=3.4
+  fi
 	conda install --yes numpy scipy matplotlib scikit-image pip
 	# Let conda install boost (so that boost_python matches)
 	conda install --yes -c https://conda.binstar.org/menpo boost=1.56.0
